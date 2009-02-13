397da9168e25198ac049df1c97f2c522ca153f34
Martin Avanzini <martin.avanzini@uibk.ac.at>
2009-02-13 10:27:22 +0100

    einrueckung
    darcs-hash:20090213092722-21975-8d53d7c80a4fa744906ffea2503f54cf9069be42.gz

--- Termlib/Substitution.hs
+++ Termlib/Substitution.hs
@@ -1,16 +1,16 @@
 module Termlib.Substitution
   (
-  empty,
-  singleton,
-  map,
-  union,
-  lookup,
-  apply,
-  compose,
-  subsumes,
-  match,
-  variant,
-  Substitution
+   empty,
+   singleton,
+   map,
+   union,
+   lookup,
+   apply,
+   compose,
+   subsumes,
+   match,
+   variant,
+   Substitution
   ) where
 
 import qualified Termlib.Term as T
@@ -38,12 +38,12 @@ compose s@(Substitution s') t@(Substitution t') = (map (apply t) s) `union` t
 s `subsumes` t = Maybe.isJust (match t s empty)
 
 match s (T.Var x) sub = Maybe.maybe (Just (compose sub (singleton x s)))
-  (\t -> if s == t then Just sub else Nothing) (lookup x sub)
-
+                        (\t -> if s == t then Just sub else Nothing) (lookup x sub)
+                                               
 match (T.Fun g ys) (T.Fun f xs) sub
-    | f == g && length xs == length ys = foldr doSubTerm (Just sub) (zip ys xs)
-    | otherwise                        = Nothing
-    where doSubTerm (y, x) s' = maybe Nothing (\sub' -> match y x sub') s'
+  | f == g && length xs == length ys = foldr doSubTerm (Just sub) (zip ys xs)
+  | otherwise                        = Nothing
+  where doSubTerm (y, x) s' = maybe Nothing (\sub' -> match y x sub') s'
 match _ _ _ = Nothing
 
 variant s t = (s `subsumes` t) && (t `subsumes` s)
