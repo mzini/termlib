407e1b2b816af392205237afdbf06cc9e9934a9a
martin.avanzini <martin.avanzini@uibk.ac.at>
2009-07-15 19:34:33 +0200

    - innermost steps implemented
    Ignore-this: a2527e5fcf57ec7ee564ba80a744a87d
    - collapsing still missing
    
    darcs-hash:20090715173433-3c1c7-db7fe4238d57bbfc2b86f00f0d0c84b9202e4584.gz

--- Termlib/TermGraph.hs
+++ Termlib/TermGraph.hs
@@ -4,10 +4,11 @@ import qualified Data.Set as Set
 import Data.Set(Set)
 import qualified Data.Map as Map
 import Data.Map(Map)
+import qualified Data.List as List
 import Data.Maybe(fromMaybe)
 import qualified Control.Monad.State.Lazy as State
 import Control.Monad.State.Lazy (State)
-
+import Control.Monad (liftM, foldM, msum)
 
 import qualified Termlib.Term as Term
 import Termlib.Term (Term(..))
@@ -16,59 +17,75 @@ import Termlib.Variable (Variable)
 import qualified Termlib.FunctionSymbol as Fun
 import Termlib.FunctionSymbol (Symbol)
 
-data HyperEdgeLabel = VL Variable 
+
+data EdgeLabel = VL Variable 
                     | FL Symbol deriving (Eq, Ord, Show)
 
-newtype HyperNode = HyperNode {nodeId :: Int} deriving (Eq, Ord, Show)
+newtype Node = Node {nodeId :: Int} deriving (Eq, Ord, Show)
 
-data HyperEdge = HyperEdge { target  :: HyperNode
-                           , sources :: [HyperNode]
-                           , label   :: HyperEdgeLabel
-                           } deriving (Eq, Ord, Show)
+data Edge = Edge { target  :: Node
+                 , sources :: [Node]
+                 , label   :: EdgeLabel
+                 } deriving (Eq, Ord, Show)
 
-mkNode :: Int -> HyperNode
-mkNode = HyperNode
+mkNode :: Int -> Node
+mkNode = Node
 
-mkEdge :: HyperNode -> [HyperNode] -> HyperEdgeLabel -> HyperEdge
-mkEdge = HyperEdge
+mkEdge :: Node -> [Node] -> EdgeLabel -> Edge
+mkEdge = Edge
 
-mkVariable :: HyperNode -> Variable -> HyperEdge
+mkVariable :: Node -> Variable -> Edge
 mkVariable r v = mkEdge r [] $ VL v
 
-mkFun :: HyperNode -> [HyperNode] -> Symbol -> HyperEdge
+mkFun :: Node -> [Node] -> Symbol -> Edge
 mkFun r cs v = mkEdge r cs $ FL v
 
-attached :: HyperEdge -> [HyperNode]
+attached :: Edge -> [Node]
 attached e =  target e : sources e
 
+isVariable :: TermGraph -> Node -> Bool
+isVariable g n = case label `liftM` edge n g of 
+                   Just (VL x) -> True
+                   _           -> False
 
-data TermGraph = TermGraph {root :: HyperNode
-                           , edges :: Map HyperNode HyperEdge
+isFun :: TermGraph -> Node -> Bool
+isFun g n = case label `liftM` edge n g of 
+              Just (FL x) -> True
+              _           -> False
+
+isDangling :: TermGraph -> Node -> Bool
+isDangling g n = case label `liftM` edge n g of 
+                   Just _ -> False
+                   _      -> True
+
+data TermGraph = TermGraph {root :: Node
+                           , edges :: Map Node Edge
                            } 
 
-nodes :: TermGraph -> Set HyperNode
+nodes :: TermGraph -> Set Node
 nodes g = root g `Set.insert` Set.fromList [ n | e <- Map.elems $ edges g, 
                                                       n <- attached e]
 
-data St = St { varNodes :: Map Variable HyperNode
-             , newId :: Int}
 
-getTarget :: Variable -> State St HyperNode
+data St a = St { content :: a
+               , newId :: Int}
+
+getTarget :: Variable -> State (St (Map Variable Node)) Node
 getTarget v = do s <- State.get
-                 case Map.lookup v (varNodes s) of
+                 case Map.lookup v (content s) of
                    Just n -> return n
                    Nothing -> fresh s
     where fresh (St m i) = do let n = mkNode (i + 1)
                               State.put $ St (Map.insert v n m) (i+1)
                               return n
 
-freshNode :: State St HyperNode
+freshNode :: State (St a) Node
 freshNode = do s <- State.get
                let i = newId s
                State.put s{newId = i + 1}
                return $ mkNode i
 
-fromTermM :: Term -> State St TermGraph
+fromTermM :: Term -> State (St (Map Variable Node)) TermGraph
 fromTermM (Var x) = do n <- getTarget x 
                        return $ TermGraph n (Map.singleton n $ mkVariable n x)
 
@@ -82,20 +99,20 @@ fromTermM (Fun f ts) = do n <- freshNode
 fromTerm :: Term -> TermGraph
 fromTerm t = fst $ State.runState (fromTermM t) (St Map.empty 0)
 
-edge :: HyperNode -> TermGraph -> HyperEdge
-edge r g = fromMaybe (error "TermGraph.edge") (Map.lookup r $ edges g)
+edge :: Node -> TermGraph -> Maybe Edge
+edge r g = Map.lookup r $ edges g
 
 toTerm :: TermGraph -> Term
 toTerm g = toTerm' (root g)
     where toTerm' r = case label e of
                         VL x -> Var x
                         FL f -> Fun f $ map toTerm' (sources e)
-              where e = edge r g
+              where e = fromMaybe (error "TermGraph.toTerm") $ edge r g
 
 garbageCollect :: TermGraph -> TermGraph
 garbageCollect g = g {edges = gc (root g)}
     where gc n = Map.insert n e $ Map.unions [gc m | m <- sources e]
-              where e = edge n g
+              where e = fromMaybe (error "TermGraph.toTerm") $  edge n g
 
 
 type Position = [Int]
@@ -114,42 +131,102 @@ prepend = (:)
 
 
 directSubgraphs :: TermGraph -> [TermGraph]
-directSubgraphs g = map mk (sources $ edge (root g) g)
+directSubgraphs g = map mk (sources $ rootedEdge g)
     where mk n = TermGraph n (edges g)
 
-subgraphAt :: TermGraph -> Position -> TermGraph
-subgraphAt g []     = g
-subgraphAt g (n:ns) = subgraphAt (g {root = src}) ns
-    where src = (sources $ edge (root g) g)!!n
+subgraphAt :: TermGraph -> Node -> Maybe TermGraph
+subgraphAt g n = case Map.member n (edges g) of 
+                   True -> Just g{root = n}
+                   False -> Nothing
 
-type Strategy = TermGraph -> Maybe Position
+
+rootedEdge :: TermGraph -> Edge
+rootedEdge g = fromMaybe (error "TermGraph.rootedEdge") $ edge (root g) g
+
+
+-- edgeAt :: TermGraph -> Position -> Maybe Edge
+-- edgeAt g p = rootedEdge `liftM` subgraphAt g p
+
+
+-- nodeAt :: TermGraph -> Position -> Maybe Node
+-- nodeAt g p = root `liftM` subgraphAt g p
+
+
 
 data TermGraphRule = TGR {lhs :: TermGraph, rhs :: TermGraph}
 
-type TGS = [TermGraphRule]
+newtype TGS = TGS {rules :: [TermGraphRule]}
+
+type Match = Map Variable Node
+
+match :: TermGraph -> (TermGraph, Node) -> Maybe Match
+match pattern (graph,node) = do redex <- subgraphAt graph node
+                                matchRoot Map.empty (redex,pattern)
+    where matchRoot m (g,p) = 
+              case label $ rootedEdge p of 
+                VL v -> case Map.lookup v m of 
+                          Just n  -> if n == (root g) 
+                                     then Just m
+                                     else Nothing
+                          Nothing -> Just $ Map.insert v (root g) m
+
+                FL f -> if rootedEdge g `eEq` rootedEdge p
+                        then foldM matchRoot m $ zip (directSubgraphs g) (directSubgraphs p)
+                        else Nothing
+                    where eEq e1 e2 = label e1 == label e2
+                                      && length (sources e1) == length (sources e2)
+
+
+matches :: TermGraph -> (TermGraph,Node) -> Bool
+matches pattern m = case match pattern m of 
+                            Just _ -> True
+                            _      -> False
+
+isRedex :: TGS -> TermGraph -> Bool
+isRedex tgs graph = any (\ lhs -> matches lhs (graph,root graph)) [lhs r | r <- rules tgs]
+
+
+type Strategy = TermGraph -> Maybe Node
 
 innermost :: TGS -> Strategy 
-innermost = undefined
+innermost tgs g = case msum [innermost tgs gi | gi <- directSubgraphs g, 
+                             c <- List.nub $ sources $ rootedEdge gi ] of
+                Nothing -> if isRedex tgs g
+                           then Just (root g)
+                           else Nothing
+                a       -> a
 
 
-type Match = Map Variable HyperNode
+replace :: (TermGraph, Match) ->  (TermGraph, Node) -> TermGraph
+replace (new, match) (tg, node) = garbageCollect $ tg {edges = Map.delete node (edges tg) `Map.union` edges'}
+    where root'     = fromMaybe (error "TermGraph.replace") $ Map.lookup (root new) m 
 
-match :: TermGraph -> TermGraph -> Maybe Match
-match g p = match' Map.empty g p 
-    where match' m g p = case label $ edge (root p) p of 
-                           VL v -> matchVar m g v
-                           FL f -> matchFun m g f
-          matchVar m g v = case Map.lookup v m of 
-                             Just n  -> if n == (root g) 
-                                        then Just m
-                                        else Nothing
-                             Nothing -> Just $ Map.insert v (root g) m
-          matchFun m g f = if label e == FL f
-                           then undefined
-                           else undefined
-              where e = edge (root g) g
-          me e1 e2 = label e1 == label e2
-                     && length (sources e1) == length (sources e2)
+          (edges',(St m _)) = State.runState wireNew (St (Map.singleton (root new) node) freshId)
+                    
+          wireNew  = foldM 
+                     (\ es (n,e) -> 
+                          case label e of
+                            VL x -> return es
+                            FL f -> do n' <- nodeOf n
+                                       sources' <- mapM nodeOf (sources e)
+                                       return $ Map.insert n' e{target=n',  sources=sources'} es) 
+                     Map.empty $ 
+                     Map.toList (edges new)
 
-stp :: Strategy -> TermGraph -> Maybe TermGraph
-stp = undefined
+          freshId   = 1 + foldl max 0 [nodeId n | n <- Set.toList $ nodes tg] 
+          nodeOf n = case label `liftM` edge n new of 
+                       Just (VL v) -> return $ fromMaybe (error "TermGraph.replace") $ Map.lookup v match
+                       _           -> do s@(St m i) <- State.get
+                                         case Map.lookup n m of 
+                                           Just n' -> return n'
+                                           Nothing -> do let n' = mkNode i
+                                                         State.put s{content = Map.insert n n' m, newId = i + 1}
+                                                         return $ n'
+
+stp :: TGS -> Strategy -> TermGraph -> Maybe TermGraph
+stp tgs strat tg =
+    do redexNode <- strat tg
+       (rhs, match) <- msum [ (,) (rhs rule) `liftM` match (lhs rule) (tg,redexNode) | rule <- rules tgs]
+       return $ replace (rhs, match) (tg,redexNode)
+                        
+                  
