194871d665c31cd0ec4b099af74f9b90eba259c2
andreas.schnabl <andreas.schnabl@uibk.ac.at>
2009-07-15 14:22:56 +0200

    20090715a
    Unifikation von Termen implementiert - brauchen wir nun doch nicht fuer edg,
    aber vielleicht ists doch irgendwann mal nuetzlich
    
    darcs-hash:20090715122256-c6f3f-87fdbd3b556d5123eb20bcc4522e172034ab1308.gz

--- Termlib/Substitution.hs
+++ Termlib/Substitution.hs
@@ -9,15 +9,18 @@ module Termlib.Substitution
    compose,
    subsumes,
    match,
+   isUnifiable,
    encompasses,
    variant,
    Substitution
   ) where
 
+import Control.Monad.State.Lazy as State
 import qualified Termlib.Term as T
 import qualified Termlib.Variable as V
 import qualified Data.Map as Map
 import qualified Data.Maybe as Maybe
+import qualified Data.Set as Set
 import Prelude hiding (lookup, map)
 newtype Substitution = Substitution (Map.Map V.Variable T.Term)
 
@@ -46,6 +49,28 @@ match (T.Fun g ys) (T.Fun f xs) sub
     | otherwise                        = Nothing
   where doSubTerm (y, x) = maybe Nothing (\sub' -> match y x sub')
 match _ _ _ = Nothing
+
+isUnifiable :: T.Term -> T.Term -> Bool
+isUnifiable s t = State.evalState unify ([(s, t)], empty)
+
+unify :: State.State ([(T.Term, T.Term)], Substitution) Bool
+unify = do (eqs, sub) <- State.get
+           if null eqs then return True else
+             case head eqs of
+               (s, t) | s == t                                   -> do State.put (tail eqs, sub)
+                                                                       unify
+               (s@(T.Fun _ _), t@(T.Var _))                      -> do State.put ((t, s) : tail eqs, sub)
+                                                                       unify
+               (T.Fun f ss, T.Fun g ts) | f /= g                 -> return False
+                                        | length ss /= length ts -> return False
+                                        | otherwise              -> do State.put (zip ss ts ++ tail eqs, sub)
+                                                                       unify
+               (T.Var x, t) | x `Set.member` T.variables t       -> return False
+                            | otherwise                          -> do let subx = singleton x t
+                                                                       let eqs' = fmap (fmap $ apply subx) (tail eqs)
+                                                                       let sub' = compose sub subx
+                                                                       State.put (eqs', sub')
+                                                                       unify
 
 variant s t = (s `subsumes` t) && (t `subsumes` s)
 
