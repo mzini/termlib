5864e11c34f97575bd9061a25813691fd8914776
martin.avanzini <martin.avanzini@uibk.ac.at>
2009-07-16 14:16:07 +0200

    parsing of terms
    Ignore-this: 778a9fe7009c36748574b1ecf9f1d9a3
    
    darcs-hash:20090716121607-3c1c7-61657f4f9da6217cc883b030c6662ecc49c0e5a3.gz

--- Termlib/Repl.hs
+++ Termlib/Repl.hs
@@ -1,14 +1,22 @@
 module Termlib.Repl where
 import qualified Termlib.Problem.Parser as PParser
-import Termlib.Problem (Problem, strictTrs)
+import qualified Termlib.Term.Parser as TParser
+import Termlib.Problem (Problem, strictTrs, variables, signature)
 import Termlib.Trs (Trs)
+import Termlib.Term (Term)
 import Control.Monad (liftM)
-loadProblem :: String -> IO (Maybe Problem) 
+
+loadProblem :: String -> IO Problem
 loadProblem input = do parsed <- PParser.problemFromFile input
                        case parsed of 
-                         Right (prob,_) -> return $ Just $ prob
-                         _              -> return $ Nothing                 
+                         Right (prob,_) -> return $ prob
+                         _              -> error "Repl.loadProblem"
                          
-loadTrs :: String -> IO (Maybe Trs) 
+loadTrs :: String -> IO Trs
 loadTrs input = do p <- loadProblem input
-                   return $ strictTrs `liftM` p
+                   return $ strictTrs p
+
+termFromString :: Problem -> String -> Term
+termFromString prob str = case TParser.termFromString (signature prob) str of 
+                            Right (t,_) -> t
+                            Left e      -> error "Repl.termFromString"
