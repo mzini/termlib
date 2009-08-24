3cfee6a56a75a1056518ef3286685e40323ef651
martin.avanzini <martin.avanzini@uibk.ac.at>
2009-08-24 15:06:04 +0200

    apply :: Trs -> ArgumentFiltering -> (Trs, Signature)
    Ignore-this: 4475088bbe0d62625a5b820f99671c10
    
    darcs-hash:20090824130604-3c1c7-922685094f9ecf6a46e5b3c19fd6dc14c873e0bb.gz

--- Termlib/ArgumentFiltering.hs
+++ Termlib/ArgumentFiltering.hs
@@ -1,13 +1,18 @@
 module Termlib.ArgumentFiltering 
 where
 import qualified Data.IntSet as Set
-import Data.IntSet (IntSet)
+import Data.IntSet (IntSet, fromList)
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Text.PrettyPrint.HughesPJ hiding (empty)
 import qualified Text.PrettyPrint.HughesPJ as PP
-import Termlib.FunctionSymbol (Signature, Symbol)
+import Termlib.FunctionSymbol (Signature, Symbol, emptySignature, arity, maybeFresh, defaultAttribs, symbolName)
 import Termlib.Utils (PrettyPrintable(..))
+import Termlib.Trs (fromRules, Trs, rules)
+import Termlib.Signature (runSignature,  getSignature)
+import Control.Monad (liftM, liftM2)
+import Termlib.Rule (Rule(..))
+import Termlib.Term (Term (..))
 
 data Filtering = Projection Int
                | Filtering IntSet deriving (Eq, Show)
@@ -18,9 +23,15 @@ newtype ArgumentFiltering = AF (Signature, Map Symbol Filtering)
 empty :: Signature -> ArgumentFiltering
 empty sig = AF (sig, Map.empty)
 
+filtering :: Symbol -> ArgumentFiltering -> Filtering
+filtering f (AF (sig,m)) = case Map.lookup f m of 
+                             Just e  -> e
+                             Nothing -> Filtering $ fromList (take ar [1..])
+                                 where ar = arity sig f
+
 instance PrettyPrintable ArgumentFiltering where 
   pprint (AF (sig, m)) | m == Map.empty = text "empty"
-                       | otherwise      = hsep $ punctuate (text ",")  [ppp s f | (s,f) <- Map.toList m]
+                       | otherwise      = fsep $ punctuate (text ",")  [ppp s f | (s,f) <- Map.toList m]
     where ppp s f = text "pi" <> parens (pprint (s, sig)) <+> text "=" <+> ppf f 
           ppf (Projection i) = text $ show i
           ppf (Filtering is) = brackets $ sep $ punctuate (text ",") $ [ text $ show i | i <- Set.toList $ is]
@@ -28,3 +39,15 @@ instance PrettyPrintable ArgumentFiltering where
 alter :: (Maybe Filtering -> Maybe Filtering) -> Symbol -> ArgumentFiltering -> ArgumentFiltering
 alter f s (AF (sig, m)) = AF (sig, Map.alter f s m)
 
+apply :: Trs -> ArgumentFiltering -> (Trs, Signature)
+apply trs af = runSignature (fromRules `liftM` mapM filterRule (rules trs)) emptySignature
+    where filterRule (Rule lhs rhs) = liftM2 Rule (filter lhs) (filter rhs)
+          filter (Var x)    = return $ Var x
+          filter (Fun f ts) = case filtering f af of 
+                                Projection i -> filter $ ts!!(i-1)
+                                Filtering is -> do {f' <- mkFreshSym;
+                                                   ts'' <- mapM filter ts';
+                                                   return $ Fun f' ts''}
+                                    where ts'        = snd $ foldl (\ (i,ts') ti -> if Set.member i is then (i+1,ti:ts') else (i+1,ts))  (1,[]) ts
+                                          mkFreshSym = do sig <- getSignature 
+                                                          maybeFresh $ defaultAttribs (symbolName sig f) (length ts')
