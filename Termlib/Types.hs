fd5990970a57b33842c6da654d24a6bbc91fff52
Martin Avanzini <martin.avanzini@uibk.ac.at>
2014-01-17 15:27:05 +0100

    initial version of typing

---
+++ Termlib/Types.hs
@@ -0,0 +1,124 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-
+This file is part of the Haskell Term Rewriting Library.
+
+The Haskell Term Rewriting Library is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The Haskell Term Rewriting Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the Haskell Term Rewriting Library.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
+module Termlib.Types 
+    (
+     Type
+    , TypeDecl (..)
+    , Typing (..)
+    , infer
+    )
+where
+
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Data.Maybe (fromJust)
+import Control.Monad.Writer
+import Control.Monad.Error
+import Control.Monad.RWS
+import Text.PrettyPrint.HughesPJ hiding (empty)
+
+import qualified Termlib.FunctionSymbol as F
+import qualified Termlib.Variable as V
+import qualified Termlib.Substitution as Subst
+import qualified Termlib.Signature as Sig
+import Termlib.Term
+import qualified Termlib.Rule as R
+import Termlib.Utils 
+
+type Type a = a
+
+data TypeDecl a = TypeDecl [Type a] (Type a)
+type Typing a = Map.Map F.Symbol (TypeDecl a)
+
+
+type UP = [(Type Int, Type Int)]
+
+newtype InferM a = 
+  InferM { run :: RWS () UP Int a }
+  deriving (Monad, MonadWriter UP, MonadState Int)
+
+(=~) :: Type Int -> Type Int -> InferM ()
+a =~ b = tell [(a,b)]
+
+infer :: F.Signature -> [R.Rule] -> Typing Int
+infer sig rs = instDecl `Map.map` ds
+    where 
+      instDecl (TypeDecl its ot) = TypeDecl [unifier `apply` t | t <- its] (unifier `apply` ot)
+
+      (ds,_,up) = runRWS (run inferM) () 0
+
+      mkDecl at (sym,attribs) = do 
+        its <- mapM (const fresh) [1.. F.symArity attribs]
+        ot <- fresh
+        return $ Map.insert sym (TypeDecl its ot) at
+
+      inferM = do 
+        decls <- foldM mkDecl Map.empty $ Sig.toList sig
+        typeRule decls `mapM` rs
+        return decls
+
+      fresh = do { i <- get; put (i + 1); return i}
+
+      typeRule decls rl = do 
+        let vars = Set.toList (R.variables rl)
+        env <- foldM (\ e v -> flip (Map.insert v) e `liftM` fresh) Map.empty vars
+        l <- typeTerm decls env (R.lhs rl)
+        r <- typeTerm decls env (R.rhs rl)
+        l =~ r
+
+      typeTerm _ env (Var v) = return $ lookupEnv env v
+      typeTerm decls env (Fun f ts) = do 
+                 its' <- typeTerm decls env `mapM` ts
+                 sequence_ [ t1 =~ t2 | (t1,t2) <- zip its' its ]
+                 return ot
+         where TypeDecl its ot = lookupDecl decls f
+
+
+      lookupEnv env v = fromJust (Map.lookup v env)
+      lookupDecl decls f = fromJust (Map.lookup f decls)
+
+      unifier = unify up
+          where 
+            unify [] = Map.empty
+            unify ((t1,t2):ts)
+                | t1 == t2 = unify ts
+                | otherwise = s `compose` unify [(s `apply` t3,s `apply` t4) | (t3,t4) <- ts]
+                where s = Map.singleton t1 t2
+            -- f subst (t1,t2) = Map.fromList [(t1,t1),(t2,t1)] `compose` subst
+            s1 `compose` s2 = (apply s2 `Map.map` s1) `Map.union` s2 -- left-biased
+
+      subst `apply` t = 
+          case Map.lookup t subst of 
+            Just t' -> t'
+            Nothing -> t
+
+instance PrettyPrintable (TypeDecl (Type Int)) where
+    pprint (TypeDecl its os) 
+        | null its = ppType os
+        | otherwise = parens ppis <+> text "->" <+> ppType os
+        where 
+          ppis = hcat $ punctuate (text ",") [ppType i | i <- its]
+          ppType = text . show
+
+instance PrettyPrintable (Typing (Type Int), F.Signature) where
+    pprint (typing, sig) 
+        | Map.null typing = text "empty"
+        | otherwise       =  vcat [pp f tp | (f,tp) <- Map.toList typing ]
+        where pp f tp = pprint (f,sig) <+> text "::" <+> pprint tp
